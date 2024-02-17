b8a9c32f6ccfca50a1a3d15c3d0aa828a96ea68d
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-02-17 12:16:44 +0000

    feat:Add file to implement basic math operations(+,-,x,/)

---
+++ basicCalculator.js
@@ -0,0 +1,24 @@
+//Create a set of functions for basic calculator operations (addition, subtraction, multiplication, division). Implement functions that take two numbers and an operator as input and return the result of the operation.
+
+function addition(num1,num2){
+    return num1 + num2;
+}
+let add = addition(10,3)
+console.log(add);
+
+function subtraction(num1,num2){
+    return num1 - num2;
+}
+let subtract = subtraction(10,3)
+console.log(subtract);
+function multiplication(num1,num2){
+    return num1 * num2;
+}
+let multiply = multiplication(10,3)
+console.log(multiply);
+function division(num1,num2){
+    return num1 / num2;
+}
+let divide = division(10,3)
+console.log(divide);
+
